cdcec038696e18d1fcc57d08d784937d2e8fd1d1
Mariam <maryammohamed61@gmail.com>
2021-06-23 23:22:13 +0200

    fix

--- src/data/data_utils/calib_reader.py
+++ src/data/data_utils/calib_reader.py
@@ -9,7 +9,7 @@ class CalibReader:
     def read_calib(self):
         lines = []
         with open(self.calib_path) as calib_file:
-            lines = calib_file.encode('utf-8').strip().readlines()
+            lines = calib_file.readlines()
         lines = list(filter(lambda x: len(x.split()) > 0, lines))
         calib_data = dict(list(map(lambda x: (x.split()[0][:-1], np.array(list(map(float, x.split()[1:])))), lines)))
         return calib_data
