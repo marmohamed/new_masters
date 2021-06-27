92635674fbf8f4d428bd96997cd485a655320736
Mariam <maryammohamed61@gmail.com>
2021-06-27 21:04:23 +0200

    fix

--- src/models/ResnetLidarBEV.py
+++ src/models/ResnetLidarBEV.py
@@ -12,6 +12,7 @@ class ResNetLidarBEV(ResNet):
     def __network(self, x, reuse=False):
            
             residual_list = [2, 4, 8, 12, 12]
+            use_deconv = False
 
             for i in range(residual_list[0]) :
                 x = conv(x, 64, kernel=3, stride=1, use_bias=True, scope='conv0_'+str(i))
@@ -38,9 +39,9 @@ class ResNetLidarBEV(ResNet):
                 downsample_arg = (i == 0)
                 x = resblock(x, channels=192,  downsample=downsample_arg, scope='resblock3_' + str(i))
 
-            x = upsample(x, size=(2, 2), scope='resnet_bev_upsample_2', use_deconv=True, filters=128, kernel_size=4)
+            x = upsample(x, size=(2, 2), scope='resnet_bev_upsample_2', use_deconv=use_deconv, filters=128, kernel_size=4)
             
-            res_groups[-1] = conv(res_groups[-1], 128, kernel=1, stride=1, scope='conv2_' + str(i))
+            res_groups[-1] = conv(res_groups[-1], 192, kernel=1, stride=1, scope='conv2_' + str(i))
             res_groups[-1] = batch_norm(res_groups[-1], scope='bn2_' + str(i))
             res_groups[-1] = relu(res_groups[-1])
 
@@ -55,9 +56,9 @@ class ResNetLidarBEV(ResNet):
                 downsample_arg = (i == 0)
                 x = resblock(x, channels=256, downsample=downsample_arg, scope='resblock4_' + str(i))
 
-            x = upsample(x, size=(2, 2), scope='resnet_bev_upsample_3', use_deconv=True, filters=128, kernel_size=4)
+            x = upsample(x, size=(2, 2), scope='resnet_bev_upsample_3', use_deconv=use_deconv, filters=128, kernel_size=4)
 
-            res_groups[-1] = conv(res_groups[-1], 128, kernel=1, stride=1, scope='conv3_' + str(i))
+            res_groups[-1] = conv(res_groups[-1], 256, kernel=1, stride=1, scope='conv3_' + str(i))
             res_groups[-1] = batch_norm(res_groups[-1],  scope='bn3_' + str(i))
             res_groups[-1] = relu(res_groups[-1])
 
