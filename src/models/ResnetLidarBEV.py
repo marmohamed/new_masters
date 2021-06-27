0574a13d4745db1793b69ae504df8091ef2a6ada
Mariam <maryammohamed61@gmail.com>
2021-06-27 20:17:22 +0200

    fix

--- src/models/ResnetLidarBEV.py
+++ src/models/ResnetLidarBEV.py
@@ -12,9 +12,10 @@ class ResNetLidarBEV(ResNet):
     def __network(self, x, reuse=False):
            
             residual_list = [2, 4, 8, 12, 12]
+            use_deconv = False
 
             for i in range(residual_list[0]) :
-                x = conv(x, 64, kernel=3, stride=1, use_bias=True, scope='conv0_'+str(i))
+                x = conv(x, 32, kernel=3, stride=1, use_bias=True, scope='conv0_'+str(i))
                 x = batch_norm(x, scope='bn_res0_'+str(i))
                 x = relu(x)
             
@@ -38,7 +39,7 @@ class ResNetLidarBEV(ResNet):
                 downsample_arg = (i == 0)
                 x = resblock(x, channels=192,  downsample=downsample_arg, scope='resblock3_' + str(i))
 
-            x = upsample(x, size=(2, 2), scope='resnet_bev_upsample_2', use_deconv=True, filters=128, kernel_size=4)
+            x = upsample(x, size=(2, 2), scope='resnet_bev_upsample_2', use_deconv=use_deconv, filters=128, kernel_size=4)
             
             res_groups[-1] = conv(res_groups[-1], 128, kernel=1, stride=1, scope='conv2_' + str(i))
             res_groups[-1] = batch_norm(res_groups[-1], scope='bn2_' + str(i))
@@ -55,7 +56,7 @@ class ResNetLidarBEV(ResNet):
                 downsample_arg = (i == 0)
                 x = resblock(x, channels=256, downsample=downsample_arg, scope='resblock4_' + str(i))
 
-            x = upsample(x, size=(2, 2), scope='resnet_bev_upsample_3', use_deconv=True, filters=128, kernel_size=4)
+            x = upsample(x, size=(2, 2), scope='resnet_bev_upsample_3', use_deconv=use_deconv, filters=128, kernel_size=4)
 
             res_groups[-1] = conv(res_groups[-1], 128, kernel=1, stride=1, scope='conv3_' + str(i))
             res_groups[-1] = batch_norm(res_groups[-1],  scope='bn3_' + str(i))
